d723e0b0dc5643228b2df0bec5c68bcc102226ed
Alex Sindledecker <alexsindledecker1@gmail.com>
2022-04-14 23:13:32 -0700

    Project setup for SFML and system compatibility

---
+++ CPTS_122-Final-Project/main.cpp
@@ -0,0 +1,31 @@
+/*
+* Class: CPTS_122
+* Authors: Alex Sindledecker, ...
+* Date-Created: 4/14/22
+* Description: Does stuff (sfml test right now)
+*/
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
